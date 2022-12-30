ff3c019924c983313436d9ad26c302bc957108c6
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-12-30 19:33:22 +0100

    Add declaration file for TypeScript

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+declare namespace Circularray {
+  class Node<Type> {
+    value: Type
+    prev: Node<Type>
+    next: Node<Type>
+
+    constructor(value: Type)
+
+    remove(): void
+    insertBetween(prev: Node<Type>, next: Node<Type>): void
+  }
+
+  export class Circularray<Type> {
+    size: number
+    pointer: Node<Type>
+
+    constructor(values: Type[])
+
+    get length(): number
+    set length(size: number)
+
+    push(value: Type): this
+    unshift(value: Type): this
+    pop(): Type
+    shift(): Type
+    rotate(offset: number): this
+    toArray(direction: 'next' | 'prev'): Type[]
+  }
+}
